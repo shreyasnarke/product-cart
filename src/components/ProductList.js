da3c6451c976128833707f3859afaaf706fed5b1
shreyasnarke <narkeshreyas44@gmail.com>
2023-04-22 15:04:01 +0530

    4/22/2023 second

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -9,15 +9,28 @@ function ProductList(props){
     return(
         <div className="product-table">
             <div className="table-data">
-                {tableHeader.map(index,label)=>{
+                {tableHeader.map((index,label)=>{
                     return( 
-                        <div className='header-text' key={'${headerText}${label}'}
-                        >{'{$index}'}</div>
+                        <div className='header-text' key={'${headerText}${label}'}>
+                        {'{$index}'}
+                        </div>
                     )
 
-                }};
+                })};
             </div>
-        </div>
-    )
+        <div className="table">
+        {filterData?.map((data, index) => {
+          return (
+            <div key={`${headerText}${index}`} className="table-container">
+              <div className="id">{`${data.id}`}</div>
+              <div className="title">{`${data.title}`}</div>
+              <div className="description">{`${data.description}`}</div>
+              <div className="price">{`${data.price}`}</div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+    );
 }
 export default ProductList;
